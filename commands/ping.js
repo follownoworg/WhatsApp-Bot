138065ba9fb04934fcde61b573c8a3da3f040d6e
follownoworg <follownoworg1@gmail.com>
2025-09-13 01:02:48 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,14 @@
 module.exports = {
-  name: "!ping",
-  aliases: ["ping"],
+  name: "!اختبار",
+  aliases: ["اختبار", "ping", "!ping"],
   run: async ({ sock, msg }) => {
     const chatId = msg.key.remoteJid;
     const ts = (msg.messageTimestamp || Math.floor(Date.now() / 1000)) * 1000;
     const latency = Date.now() - ts;
-    await sock.sendMessage(chatId, { text: `🏓 Pong! ~${latency >= 0 ? latency : 0}ms` }, { quoted: msg });
+    await sock.sendMessage(
+      chatId,
+      { text: `🏓 اختبار الاستجابة: ~${latency >= 0 ? latency : 0} مللي ثانية` },
+      { quoted: msg }
+    );
   }
 };
