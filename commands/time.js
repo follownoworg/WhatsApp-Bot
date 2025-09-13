b253a9b1c888a40fd183a89d5e01b323e294e615
follownoworg <follownoworg1@gmail.com>
2025-09-13 19:32:37 +0300

    Update time.js

--- commands/time.js
+++ commands/time.js
@@ -1,9 +1,10 @@
+// commands/time.js
 module.exports = {
-  name: "!الوقت",
-  aliases: ["الوقت", "time", "!time"],
+  name: "الوقت",
+  aliases: ["الساعة", "التاريخ"],
   run: async ({ sock, msg }) => {
     const chatId = msg.key.remoteJid;
-    const now = new Date().toLocaleString("ar-EG", { timeZone: "Asia/Riyadh" }); // الوقت بالعربية
-    await sock.sendMessage(chatId, { text: `🕒 الوقت الحالي: ${now}` }, { quoted: msg });
-  }
+    const now = new Date().toLocaleString("ar-YE", { timeZone: "Asia/Aden" });
+    await sock.sendMessage(chatId, { text: `🕒 الوقت الحالي (آسيا/عدن): ${now}` }, { quoted: msg });
+  },
 };
